a3a8b75d7cc95148bf0d23afa8d1b4d793d18575
EricDai0912 <eric.dai.au@gmail.com>
2024-08-07 12:35:21 +1000

    Finalise java rolling project

--- Java/Rolling Project/Enrolment.java
+++ Java/Rolling Project/Enrolment.java
@@ -9,7 +9,8 @@ public class Enrolment {
         date = "No date";
     }
 
-    public Enrolment(String date, Student student, Unit unit) {
+    public Enrolment(String date, Student student, Unit unit)
+    {
         this.date = date;
         this.student = student;
         this.unit = unit;
@@ -20,36 +21,61 @@ public class Enrolment {
         System.out.println("Date: " + date + " student: " + student + " unit: " + unit);
     }
 
-    public String getDate() {
+    public String getDate()
+    {
         return date;
     }
 
-    public Student getStudent() {
+    public Student getStudent()
+    {
         return student;
     }
 
-    public Unit getUnit() {
+    public Unit getUnit()
+    {
         return unit;
     }
 
-    public void setDate(String date) {
+    public void setDate(String date)
+    {
         this.date = date;
     }
 
-    public void setStudent(Student student) {
+    public void setStudent(Student student)
+    {
         this.student = student;
     }
 
-    public void setUnit(Unit unit) {
+    public void setUnit(Unit unit)
+    {
         this.unit = unit;
     }
 
+    public void inputStudentDetails()
+    {
+        Input in = new Input();
+        student.setName(in.acceptStringInput("Please enter your name"));
+        student.setAddress(in.acceptStringInput("Please enter your address"));
+        student.setPhoneNo(in.acceptStringInput("Please enter your phone number"));
+        student.setEmail(in.acceptStringInput("Please enter your email"));
+    }
+
+    public void inputUnitDetails()
+    {
+        Input in = new Input();
+        unit.setUnitCode(in.acceptStringInput("Please enter your unit code"));
+        unit.setUnitDescription(in.acceptStringInput("Please enter your unit description"));
+        unit.setCreditPoint(in.acceptIntInput("Please enter your credit points"));
+    }
+
     @Override
-    public String toString() {
+    public String toString()
+    {
         return "Enrolment [date=" + date + ", student=" + student + ", unit=" + unit + "]";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         
     }
 
